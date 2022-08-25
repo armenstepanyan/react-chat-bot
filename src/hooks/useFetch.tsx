c443b8be93d5cbedf98d1073b54834297f395dfb
Armen Stepanyan <st.armen91@gmail.com>
2022-08-25 11:57:24 +0300

    Work on fetch request: handle ivalid json response

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,29 +1,50 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-function useFetch <T>(url: string, initialData: any): { data: T, loading: boolean, error: boolean } {
+interface FetchOptions {
+  method?: "GET" | "POST" | "PUT" | "DELETE";
+  body?: string;
+}
+
+function useFetch<T>( url: string, initialData: any,  options?: FetchOptions): { data: T; loading: boolean; error: boolean } {
   const [data, setData] = useState(initialData);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
+  const { method = "GET", body = "" } = options || {};
+
   useEffect(() => {
-    console.log('send request');
+    console.log("send request");
     setLoading(true);
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => {
-        if (res.error) {
-          setError(true);
+
+    const request$ = fetch(url, { method, ...(method !== "GET" && { body }) });
+
+    request$
+      .then(async (statusResp) => {
+        let resp;
+        if (statusResp.ok) {
+          try {
+            // in case if request was succeed and valid JSON data was returned
+            resp = await statusResp.clone().json();
+            return resp;
+          } catch (e) {
+            // in case if request was succeed but no JSON data was returned
+            resp = await statusResp.text();
+            return resp;
+          }
         } else {
-            setData(res);
+          throw new Error("Error occured");
         }
       })
-      .catch((err) => {
+      .then((res) => {
+        setData(res);
+      })
+      .catch(() => {
         setError(true);
       })
       .finally(() => setLoading(false));
   }, []);
 
   return { data, loading, error };
-};
+}
 
 export default useFetch;
